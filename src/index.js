d2ec12810589c1b6c4fb4e76f4f6f7d6238fec80
Erik Gillespie <erik.gillespie@gmail.com>
2019-05-10 22:11:44 -0400

    Generate 15 unique 4-color tiles

--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@
 
   // Create a tile
   const createTile = require('./util/create-tile')
-  const t = config.colors[0]
-  const r = config.colors[1]
-  const b = config.colors[2]
-  const l = config.colors[2]
-  await createTile({ t, r, b, l })
+  const numColors = config.colors.length
+
+  for (let iTop = 0; iTop < numColors - 3; iTop++) {
+    for (let iRight = iTop + 1; iRight < numColors - 2; iRight++) {
+      for (let iBottom = iRight + 1; iBottom < numColors - 1; iBottom++) {
+        for (let iLeft = iBottom + 1; iLeft < numColors; iLeft++) {
+          const t = config.colors[iTop]
+          const r = config.colors[iRight]
+          const b = config.colors[iBottom]
+          const l = config.colors[iLeft]
+          await createTile({ t, r, b, l })
+        }
+      }
+    }
+  }
 })()
