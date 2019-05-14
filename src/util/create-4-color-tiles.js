0622ff1a85911f449fa7d87c0fa96617f3befb6c
Erik Gillespie <erik.gillespie@gmail.com>
2019-05-13 21:48:07 -0400

    Produce different unique patterns of 4-color tiles

--- src/util/create-4-color-tiles.js
+++ src/util/create-4-color-tiles.js
@@ -1,6 +1,7 @@
 const config = require('../../package.json').config
 const createTile = require('./create-tile')
 
+// Produces 60 unique tiles
 module.exports = async () => {
   const numColors = config.colors.length
   const quantity = config.quantities[4] || 1
@@ -10,6 +11,51 @@ module.exports = async () => {
     for (let right = top + 1; right < numColors - 2; right++) {
       for (let bottom = right + 1; bottom < numColors - 1; bottom++) {
         for (let left = bottom + 1; left < numColors; left++) {
+          const t = config.colors[top]
+          const r = config.colors[right]
+          const b = config.colors[bottom]
+          const l = config.colors[left]
+          await createTile({ t, r, b, l, quantity })
+        }
+      }
+    }
+  }
+
+  // Produces 15 unique tiles
+  for (let top = 0; top < numColors - 3; top++) {
+    for (let bottom = top + 1; bottom < numColors - 2; bottom++) {
+      for (let left = bottom + 1; left < numColors - 1; left++) {
+        for (let right = left + 1; right < numColors; right++) {
+          const t = config.colors[top]
+          const r = config.colors[right]
+          const b = config.colors[bottom]
+          const l = config.colors[left]
+          await createTile({ t, r, b, l, quantity })
+        }
+      }
+    }
+  }
+
+  // Produces 15 unique tiles
+  for (let top = 0; top < numColors - 3; top++) {
+    for (let left = top + 1; left < numColors - 2; left++) {
+      for (let right = left + 1; right < numColors - 1; right++) {
+        for (let bottom = right + 1; bottom < numColors; bottom++) {
+          const t = config.colors[top]
+          const r = config.colors[right]
+          const b = config.colors[bottom]
+          const l = config.colors[left]
+          await createTile({ t, r, b, l, quantity })
+        }
+      }
+    }
+  }
+
+  // Produces 15 unique tiles
+  for (let top = 0; top < numColors - 3; top++) {
+    for (let left = top + 1; left < numColors - 2; left++) {
+      for (let bottom = left + 1; bottom < numColors - 1; bottom++) {
+        for (let right = bottom + 1; right < numColors; right++) {
           const t = config.colors[top]
           const r = config.colors[right]
           const b = config.colors[bottom]
